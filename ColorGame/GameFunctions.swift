047095d96a99df57e7c3f0adf2a05db32869b8e3
MinRiDaddy <lovelyroom77@gmail.com>
2017-12-03 18:35:24 +0900

    12월 3일 commit

---
+++ ColorGame/GameFunctions.swift
@@ -0,0 +1,113 @@
+//
+//  GameFunctions.swift
+//  ColorGame
+//
+//  Created by MinRiDaddy on 2017. 12. 3..
+//  Copyright © 2017년 MinRiDaddy. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+extension GameScene {
+    
+    func launchGameTimer () {
+        let timeAction = SKAction.repeatForever(SKAction.sequence([SKAction.run({
+            self.remainingTime -= 1
+            }), SKAction.wait(forDuration: 1)]))
+
+        timeLabel?.run(timeAction)
+    }
+    
+    // 위, 아래 이동
+    func moveVertically (up:Bool) {
+        if up {
+            // x 는 변화가 없고 y 로 3 pixel 을 0.01 시간에 이동
+            let moveAction = SKAction.moveBy(x: 0, y: 3, duration: 0.01)
+            // 위의 이동을 계속 반복하도록 설정
+            let repeatAction = SKAction.repeatForever(moveAction)
+            // 플레이어에 액션 설정
+            player?.run(repeatAction)
+        } else {
+            // x 는 변화가 없고 y 로 -3 pixel 을 0.01 시간에 이동
+            let moveAction = SKAction.moveBy(x: 0, y: -3, duration: 0.01)
+            // 위의 이동을 계속 반복하도록 설정
+            let repeatAction = SKAction.repeatForever(moveAction)
+            // 플레이어에 액션 설정
+            player?.run(repeatAction)
+        }
+    }
+    
+    func moveToNextTrack () {
+        // 다음 트랙으로 이동 전 플레이어의 액션을 모두 제거
+        player?.removeAllActions()
+        // 수평 이동 여부 세팅
+        movingToTrack = true
+        
+        // 다음 트랙의 position
+        guard let nextTrack = tracksArray?[currentTrack + 1].position else {return}
+        
+        // 플레이어 객체
+        if let player = self.player {
+            // 플레이어의 액션
+            // 다음 트랙의 x 좌표, 현재 플레이어의 y 좌표로 이동 액션
+            let moveAction = SKAction.move(to: CGPoint(x: nextTrack.x, y: player.position.y), duration: 0.2)
+            
+            let up = directionArray[currentTrack + 1]
+            
+            //player.run(moveAction)
+            // 액션 실행 : 완료되면 수평 이동 여부 세팅
+            player.run(moveAction, completion: {
+                self.movingToTrack = false
+                
+                if self.currentTrack != 8 {
+                    self.player?.physicsBody?.velocity = up ? CGVector(dx: 0, dy: self.velocityArray[self.currentTrack]) : CGVector(dx: 0, dy: -self.velocityArray[self.currentTrack])
+                }else {
+                    self.player?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                }
+                
+            })
+            // 현재 트랙 정보 갱신
+            currentTrack += 1
+            
+            // 이동 사운드 실행
+            self.run(moveSound)
+        }
+    }
+    
+    func spwanEnemies () {
+        for i in 1 ... 7 {
+            let randomEnemy = Enemies(rawValue: GKRandomSource.sharedRandom().nextInt(upperBound: 3))!
+            if let newEnemy = createEnemy(type: randomEnemy, forTrack: i) {
+                self.addChild(newEnemy)
+            }
+        }
+        
+        self.enumerateChildNodes(withName: "ENEMY") { (node: SKNode, nil) in
+            if node.position.y < -150 || node.position.y > self.size.height + 150 {
+                node.removeFromParent()
+            }
+        }
+    }
+    
+    func movePlayerToStart () {
+        if let player = self.player {
+            player.removeFromParent()
+            self.player = nil
+            self.createPlayer()
+            self.currentTrack = 0
+        }
+    }
+    
+    func nextLevel (playerPhysicsBody: SKPhysicsBody) {
+        currentScore += 1
+        self.run(SKAction.playSoundFileNamed("levelUp.wav", waitForCompletion: true))
+        let emitter = SKEmitterNode(fileNamed: "fireworks.sks")
+        playerPhysicsBody.node?.addChild(emitter!)
+        
+        self.run(SKAction.wait(forDuration: 0.5)) {
+            emitter?.removeFromParent()
+            self.movePlayerToStart()
+        }
+    }
+}
--- ColorGame/GameScene.swift
+++ ColorGame/GameScene.swift
@@ -9,19 +9,27 @@
 import SpriteKit
 import GameplayKit
 
-enum Enemies: Int {
-    case small
-    case medium
-    case large
-}
-
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // 트랙 배열
     var tracksArray:[SKSpriteNode]? = [SKSpriteNode]()
+    
     // 플레이어
     var player:SKSpriteNode?
     var target:SKSpriteNode?
+    
+    var timeLabel:SKLabelNode?
+    var scoreLabel:SKLabelNode?
+    var currentScore:Int = 0 {
+        didSet {
+            self.scoreLabel?.text = "SCORE: \(self.currentScore)"
+        }
+    }
+    var remainingTime:TimeInterval = 60 {
+        didSet {
+            self.timeLabel?.text = "TIME: \(Int(self.remainingTime))"
+        }
+    }
     
     // 현재 트랙 번호
     var currentTrack = 0
@@ -30,6 +38,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // 이동 시 sound
     let moveSound = SKAction.playSoundFileNamed("move.wav", waitForCompletion: false)
+    var backgroundNoise:SKAudioNode!
+    
     
     // velocity 값 배열
     let trackVelocities = [180, 200, 250]
@@ -38,117 +48,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // 랜덤 velocity 배열
     var velocityArray = [Int]()
     
-    let playerCategory:UInt32 = 0x1 << 0
-    let enemyCategory:UInt32 = 0x1 << 1
-    let targetCategory:UInt32 = 0x1 << 2
-    
-    
-    // 앱 실행 시 최초에 트랙 정보를 배열로 옮긴다
-    func setupTracks() {
-        for i in 0 ... 8 {
-            if let track = self.childNode(withName: "\(i)") as? SKSpriteNode {
-                tracksArray?.append(track)
-            }
-        }
-    }
-    
-    // 앱 실행 시 플레이어 객체를 생성하고 노드로 추가한다.
-    func createPlayer() {
-        player = SKSpriteNode(imageNamed: "player")
-        player?.physicsBody = SKPhysicsBody(circleOfRadius: player!.size.width / 2)
-        player?.physicsBody?.linearDamping = 0
-        player?.physicsBody?.categoryBitMask = playerCategory
-        player?.physicsBody?.collisionBitMask = 0
-        player?.physicsBody?.contactTestBitMask = enemyCategory | targetCategory
-        
-        
-        // 첫번째 트랙의 x 값
-        guard let playerPosition = tracksArray?.first?.position.x else {return}
-        // 최초 플레이어의 위치는 첫번째 트랙의 x 와 동일한 x 값을 가지고
-        // y 값은 scene 의 height 의 중간값으로 설정
-        player?.position = CGPoint(x: playerPosition, y: self.size.height / 2)
-        
-        // 노드로 추가
-        self.addChild(player!)
-        
-        let pulse = SKEmitterNode(fileNamed: "pulse")!
-        player?.addChild(pulse)
-        pulse.position = CGPoint(x: 0, y: 0)
-        
-    }
-    
-    func createTarget () {
-        target = self.childNode(withName: "target") as? SKSpriteNode
-        target?.physicsBody = SKPhysicsBody(circleOfRadius: target!.size.width / 2)
-        target?.physicsBody?.categoryBitMask = targetCategory
-        target?.physicsBody?.collisionBitMask = 0
-    }
-    
-    func createEnemy (type: Enemies, forTrack track: Int) -> SKShapeNode? {
-        // SKShapeNode 객체 생성(장애물)
-        let enemySprite = SKShapeNode()
-        enemySprite.name = "ENEMY"
-        
-        // 타입에 따라 다른 path, 칼라 설정
-        switch type {
-        case .small:
-            // width 20, height 70
-            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 70), cornerWidth: 8, cornerHeight: 8, transform: nil)
-            enemySprite.fillColor = UIColor(red: 0.4431, green: 0.5529, blue: 0.7451, alpha: 1)
-        case .medium:
-            // width 20, height 100
-            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 100), cornerWidth: 8, cornerHeight: 8, transform: nil)
-            enemySprite.fillColor = UIColor(red: 0.7804, green: 0.4039, blue: 0.4039, alpha: 1)
-        case .large:
-            // width 20, height 130
-            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 130), cornerWidth: 8, cornerHeight: 8, transform: nil)
-            enemySprite.fillColor = UIColor(red: 0.7804, green: 0.6392, blue: 0.4039, alpha: 1)
-        }
-        
-        // 트랙의 position 객체
-        guard let enemyPosition = tracksArray?[track].position else {return nil}
-        
-        // 랜덤하게 결정된 트랙의 방향
-        let up = directionArray[track]
-        
-        // 장애물의 x 좌표는 트랙의 x 좌표
-        enemySprite.position.x = enemyPosition.x
-        // 장애물의 y 좌표는 랜덤하게 결정된 방향에 따라 -130
-        // 또는 scene 의 height + 130 으로 설정
-        enemySprite.position.y = up ? -130 : self.size.height + 130
-        
-        // 장애물의 physicsBody
-        enemySprite.physicsBody = SKPhysicsBody(edgeLoopFrom: enemySprite.path!)
-        
-        enemySprite.physicsBody?.categoryBitMask = enemyCategory
-        
-        // 랜덤하게 결정된 velocity 에 따라 physicsBody 의 velocity 설정
-        enemySprite.physicsBody?.velocity = up ? CGVector(dx: 0, dy: velocityArray[track]) : CGVector(dx: 0, dy: -velocityArray[track])
-        
-        return enemySprite
-    }
-    
-    func spwanEnemies () {
-        for i in 1 ... 7 {
-            let randomEnemy = Enemies(rawValue: GKRandomSource.sharedRandom().nextInt(upperBound: 3))!
-            if let newEnemy = createEnemy(type: randomEnemy, forTrack: i) {
-                self.addChild(newEnemy)
-            }
-        }
-        
-        self.enumerateChildNodes(withName: "ENEMY") { (node: SKNode, nil) in
-            if node.position.y < -150 || node.position.y > self.size.height + 150 {
-                node.removeFromParent()
-            }
-        }
-    }
+    let playerCategory:UInt32 = 0x1 << 0 // 플레이어 카테고리 bitmask
+    let enemyCategory:UInt32 = 0x1 << 1  // 장애물 카테고리 bitmask
+    let targetCategory:UInt32 = 0x1 << 2 // 타겟 카테고리 bitmask
     
     override func didMove(to view: SKView) {
         setupTracks()
+        
+        createHUD()
+        launchGameTimer()
+        
         createPlayer()
         createTarget()
         
         self.physicsWorld.contactDelegate = self
+        
+        if let musicURL = Bundle.main.url(forResource: "background", withExtension: "wav") {
+            backgroundNoise = SKAudioNode(url: musicURL)
+            addChild(backgroundNoise)
+        }
         
         tracksArray?.first?.color = UIColor.green
         
@@ -167,52 +85,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.run(SKAction.repeatForever(SKAction.sequence([SKAction.run {
             self.spwanEnemies()
         }, SKAction.wait(forDuration: 2)])))
-    }
-    
-    // 위, 아래 이동
-    func moveVertically (up:Bool) {
-        if up {
-            // x 는 변화가 없고 y 로 3 pixel 을 0.01 시간에 이동
-            let moveAction = SKAction.moveBy(x: 0, y: 3, duration: 0.01)
-            // 위의 이동을 계속 반복하도록 설정
-            let repeatAction = SKAction.repeatForever(moveAction)
-            // 플레이어에 액션 설정
-            player?.run(repeatAction)
-        } else {
-            // x 는 변화가 없고 y 로 -3 pixel 을 0.01 시간에 이동
-            let moveAction = SKAction.moveBy(x: 0, y: -3, duration: 0.01)
-            // 위의 이동을 계속 반복하도록 설정
-            let repeatAction = SKAction.repeatForever(moveAction)
-            // 플레이어에 액션 설정
-            player?.run(repeatAction)
-        }
-    }
-    
-    func moveToNextTrack () {
-        // 다음 트랙으로 이동 전 플레이어의 액션을 모두 제거
-        player?.removeAllActions()
-        // 수평 이동 여부 세팅
-        movingToTrack = true
-        
-        // 다음 트랙의 position
-        guard let nextTrack = tracksArray?[currentTrack + 1].position else {return}
-        
-        // 플레이어 객체
-        if let player = self.player {
-            // 플레이어의 액션
-            // 다음 트랙의 x 좌표, 현재 플레이어의 y 좌표로 이동 액션
-            let moveAction = SKAction.move(to: CGPoint(x: nextTrack.x, y: player.position.y), duration: 0.2)
-            //player.run(moveAction)
-            // 액션 실행 : 완료되면 수평 이동 여부 세팅
-            player.run(moveAction, completion: {
-                self.movingToTrack = false
-                })
-            // 현재 트랙 정보 갱신
-            currentTrack += 1
-            
-            // 이동 사운드 실행
-            self.run(moveSound)
-        }
     }
     
     // touch 인식
@@ -245,10 +117,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player?.removeAllActions()
     }
     
+    // SKPhysicsContactDelegate 프로토콜 didBegin 구현
     func didBegin(_ contact: SKPhysicsContact) {
         var playerBody:SKPhysicsBody
         var otherBody:SKPhysicsBody
         
+        // bodyA 와 bodyB 를 비교해서 플레이어와 other 를 구분
         if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
             playerBody = contact.bodyA
             otherBody = contact.bodyB
@@ -257,15 +131,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             otherBody = contact.bodyA
         }
         
+        // 플레이어가 장애물과 만난것인지, 타겟과 만난것인지 구분
         if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask == enemyCategory {
-            print("Enemy hit")
+            self.run(SKAction.playSoundFileNamed("fail.wav", waitForCompletion: true))
+            movePlayerToStart()
         } else if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask == targetCategory {
-            print("Target hit")
+            nextLevel(playerPhysicsBody: playerBody)
         }
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if let player = self.player {
+            if player.position.y > self.size.height || player.position.y < 0 {
+                movePlayerToStart()
+            }
+        }
+        
+        if remainingTime <= 5 {
+            timeLabel?.fontColor = UIColor.red
+        }
     }
 }
